0efdb5c57c2529e45642c17c6193d27cf9ab8c16
alex <alex96ford19@gmail.com>
2020-05-27 14:59:30 +0100

    Moved the request import.

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 from os import path
-from flask_pymongo import PyMongo, pymongo, request
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
 if path.exists("env.py"):
@@ -11,7 +11,7 @@ app = Flask(__name__)
 
 DBURL = os.environ.get('DBURL')
 
-app.config["MONGO_DBNAME"] = 'quiz'
+app.config["MONGO_DBNAME"] = 'task_manager'
 app.config["MONGO_URI"] = DBURL
 
 
@@ -19,10 +19,62 @@ mongo = PyMongo(app)
 
 @app.route('/')
 def index():
+    alex_answers = mongo.db.alex.find()
+    mum_answers = mongo.db.mum.find()
+    joseph_answers = mongo.db.joseph.find()
+
+    return render_template('index.html', alex_answers=alex_answers,mum_answers=mum_answers ,joseph_answers=joseph_answers  )
 
 
-    return 'working'
+# save the users answers
+@app.route('/save_alex', methods=['POST'])
+def save_alex():
+    answer = request.form['answer']
+    quiz_database = mongo.db.alex
+    quiz_answer = {
+        'answer': answer
+    }
 
+    quiz_database.insert_one(quiz_answer)
+    return redirect(url_for('index'))
+
+@app.route('/save_mum', methods=['POST'])
+def save_mum():
+    answer = request.form['answer']
+    quiz_database = mongo.db.mum
+    quiz_answer = {
+        'answer': answer
+    }
+
+    quiz_database.insert_one(quiz_answer)
+    return redirect(url_for('index'))
+
+@app.route('/save_joseph', methods=['POST'])
+def save_joseph():
+    answer = request.form['answer']
+    quiz_database = mongo.db.joseph
+    quiz_answer = {
+        'answer': answer
+    }
+
+    quiz_database.insert_one(quiz_answer)
+    return redirect(url_for('index'))
+
+# delete the users answers
+@app.route('/delete_alex/<data_id>')
+def delete_alex(data_id):
+    mongo.db.alex.remove({'_id': ObjectId(data_id)})
+    return redirect(url_for('index'))
+
+@app.route('/delete_mum/<data_id>')
+def delete_mum(data_id):
+    mongo.db.mum.remove({'_id': ObjectId(data_id)})
+    return redirect(url_for('index'))
+
+@app.route('/delete_joseph/<data_id>')
+def delete_joseph(data_id):
+    mongo.db.joseph.remove({'_id': ObjectId(data_id)})
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.secret_key = 'mysecret'
