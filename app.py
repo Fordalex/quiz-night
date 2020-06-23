88da865d6be8ce8b047ff871d5099cbe2cb6c12f
alex <alex96ford19@gmail.com>
2020-06-23 22:51:57 +0100

    Cleaned up the app.py file and made the titles more consistant throughout the pages.

--- app.py
+++ app.py
@@ -15,24 +15,27 @@ app.config["MONGO_URI"] = DBURL
 
 mongo = PyMongo(app)
 
-# The setting page
-@app.route('/', methods=["GET", "POST"])
+
+# The settings page where a user can create a quiz.
+@app.route('/settings', methods=["GET", "POST"])
 def settings():
+    # If user has tried to loggin to a quiz using the 'quiz name' and the 'pin'.
     if request.method == 'POST':
         quizName = request.form['quizName']
         userCount = request.form['userCount']
         arrayOfUsers = {}
         arrayOfNames = []
 
+        # Count the amount of users and sort them into arrayOfUsers.
         for user in range(int(userCount)):
             userKey = 'user' + str(user + 1)
             usersName = request.form[userKey]
 
             aUser = {usersName : { 'answers': [], 'categories': []} }
-
             arrayOfUsers.update(aUser)
             arrayOfNames.append(usersName)
 
+        # Sorting the quiz data and sending it to the database.
         quiz = {
             'quizName': quizName,
             'pin': request.form['pin'],
@@ -43,78 +46,96 @@ def settings():
             'usersNames': arrayOfNames,
             'showAnswers': 'False',
         }
-
         mongo.db.quizzes.insert_one(quiz)
-        return redirect(url_for('user', quizName=quizName))
+        session['quizName'] = quizName
 
+        return redirect(url_for('user'))
 
+    # If viewing the page render this template.
     return render_template('settings.html')
 
-# find the quiz of your choose
-@app.route('/menu', methods=["GET","POST"])
+
+# Search for the users request quiz and return the page or return back to the menu with an error.
+@app.route('/', methods=["GET","POST"])
 def menu():
+    error = False
+    # If user is looking for a quiz search the database or return an error.
     if request.method == 'POST':
         quizName = request.form['quizName']
         pin = request.form['pin']
+
         if mongo.db.quizzes.find_one({'quizName': quizName, 'pin': pin}):
-            return redirect(url_for('user', quizName=quizName))
+            session['quizName'] = quizName
+            return redirect(url_for('user'))
 
-    return render_template('menu.html')
+        else:
+            error = "The quiz you were looking for wasn't found, check the quiz name and the pin."
+
+    return render_template('menu.html', error=error)
 
 
+# Select the username or return to the quiz.
+@app.route('/user', methods=["GET", "POST"])
+def user():
+    quizName = session['quizName']
 
-# Select the username or return to the quiz
-@app.route('/user/<quizName>', methods=["GET", "POST"])
-def user(quizName):
     if request.method == "POST":
         session["username"] = request.form['username']
 
     if "username" in session:
-        return redirect(url_for('categories', username=session["username"], quizName=quizName))
+        return redirect(url_for('categories'))
 
     quizName = mongo.db.quizzes.find_one({'quizName': quizName})
 
     return render_template('user.html', quiz=quizName)
 
 
-# Add the categories for the quiz
-@app.route('/categories/<username>/<quizName>', methods=["GET", "POST"])
-def categories(username, quizName):
+# Add the categories for the quiz.
+@app.route('/categories', methods=["GET", "POST"])
+def categories():
+    # Find the current quiz
+    quizName = session['quizName']
+    username = session['username']
     quiz = mongo.db.quizzes.find_one({'quizName': quizName})
+
+    usersCats = quiz['usersData'][username]['categories']
+
+    # If categories already in database go straight back to quiz.
+    if usersCats:
+        return redirect(url_for('index'))
+
+    # Save the categories to the database and join the quiz.
     if request.method == "POST":
-        usersCats = quiz['usersData'][username]['categories']
         catNumber = 0
         while True:
             catNumber += 1
-            print(catNumber)
             formName = 'cat' + str(catNumber)
             try:
                 cat = request.form[formName]
                 usersCats.append(cat)
-                print(cat)
                 continue
             except:
                 cat = False
                 break
 
         mongo.db.quizzes.update({'quizName': quizName},quiz)
-        
-        return redirect(url_for('index', username=session["username"], quizName=quizName))
+        return redirect(url_for('index'))
 
-    return render_template('addcategories.html', username=username, quiz=quiz)
+    return render_template('addcategories.html', quiz=quiz)
 
 
 # The quiz page with the user selected
-@app.route('/index/<username>/<quizName>')
-def index(username, quizName):
+@app.route('/index')
+def index():
+    username = session['username']
+    quizName = session['quizName']
     quizNameMongo = mongo.db.quizzes.find_one({'quizName': quizName})
-    session['quizName'] = quizName
 
-    # find out the total amount of questions going to be asked.
+    # Find out the total amount of questions going to be asked.
     userCountMinusOne = quizNameMongo['userCount'] - 1
     totalQuestionsPerPerson = quizNameMongo['categories'] * quizNameMongo['questionsCount'] * userCountMinusOne
     totalQuestions = totalQuestionsPerPerson * quizNameMongo['userCount']
-    # How many questions have been answered
+    # How many questions have been answered.
     totalAnswers = 0
     usersName = quizNameMongo['usersNames']
 
@@ -122,15 +143,11 @@ def index(username, quizName):
         usersAnswersLength = len(quizNameMongo['usersData'][user]['answers'])
         totalAnswers += usersAnswersLength
 
-    # if the total questions is the same as the questions answered
+    # If the total questions is the same as the questions answered.
     if totalAnswers == totalQuestions:
         quizFinished = True
     else:
         quizFinished = False
-
-    print(totalQuestions)
-    print(totalAnswers)
-
 
     return render_template('quiz.html', quiz=quizNameMongo, username=username, quizFinished=quizFinished)
 
@@ -148,26 +165,8 @@ def save():
 
     mongo.db.quizzes.update({'quizName': quizName},quiz)
 
-    print(quiz['usersData'][username])
-
     return redirect(url_for('index',username=username, quizName=quizName))
 
-
-# delete the users answers
-@app.route('/delete_alex/<data_id>')
-def delete_alex(data_id):
-    mongo.db.alex.remove({'_id': ObjectId(data_id)})
-    return redirect(url_for('index', username=session["username"]))
-
-@app.route('/delete_mum/<data_id>')
-def delete_mum(data_id):
-    mongo.db.mum.remove({'_id': ObjectId(data_id)})
-    return redirect(url_for('index', username=session["username"]))
-
-@app.route('/delete_joseph/<data_id>')
-def delete_joseph(data_id):
-    mongo.db.joseph.remove({'_id': ObjectId(data_id)})
-    return redirect(url_for('index', username=session["username"]))
 
 # quiz_finished
 @app.route('/quiz_finished')
@@ -182,14 +181,14 @@ def quiz_finished():
 
     return redirect(url_for('index', username=session["username"], quizName=quizName))
 
-# log out
+# Log the user out.
 @app.route('/log_out')
 def log_out():
     session.clear()
 
     return redirect(url_for('menu'))
 
-# restart quiz
+# Remove the quiz from the database.
 @app.route('/quiz_ended')
 def quiz_ended():
     # get the current quiz
